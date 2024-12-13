7d053012be7f0d23d6641b2de134cbec13f893b6
Valerii Kashpur <kashpur.v.f@gmail.com>
2024-12-13 16:50:03 +0200

    Input logger

--- app/main.py
+++ app/main.py
@@ -1,6 +1,13 @@
-def main():
-    # write your code here
-    pass
+def main() -> None:
+    file_name = input("Enter name of the file: ")
+
+    with open(file_name + ".txt", "a") as file:
+        input_value = None
+
+        while input_value != "stop":
+            input_value = input("Enter new line of content: ")
+            if input_value != "stop":
+                file.write(input_value + "\n")
 
 
 if __name__ == "__main__":
